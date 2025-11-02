eb60788c584877581f7b246b817386f9adf5df08
M-G-C-64 <mgc01official@gmail.com>
2025-11-03 02:40:53 +0530

    changed join syntax

--- rawSpark/RawSparkScript.py
+++ rawSpark/RawSparkScript.py
@@ -1,9 +1,9 @@
 import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
+from awsglue.transforms import * # pyright: ignore[reportMissingImports]
+from awsglue.utils import getResolvedOptions # pyright: ignore[reportMissingImports]
 from pyspark.context import SparkContext
-from awsglue.context import GlueContext
-from awsglue.job import Job
+from awsglue.context import GlueContext # pyright: ignore[reportMissingImports]
+from awsglue.job import Job # pyright: ignore[reportMissingImports]
 
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -28,32 +28,38 @@ job.commit()
     limit 10;
 '''
 
-# read the tabledriver_standings data
-gc_df_tds = glueContext.create_dynamic_frame_from_catalog(database="admin_prod", table_name="tabledriver_standings")
-df_tds = gc_df_tds.toDF()
+try:
 
-# read the tabledrivers
-gc_df_td = glueContext.create_dynamic_frame_from_catalog(database="admin_prod", table_name="tabledrivers")
-df_td = gc_df_td.toDF()
-
-# read the tableraces
-gc_df_tr = glueContext.create_dynamic_frame_from_catalog(database="admin_prod", table_name="tableraces")
-df_tr = gc_df_tr.toDF()
-
-# Rename some generic column names
-df_tr.withColumnsRenamed({"date":"race_date", "name":"race_name"})
-
-# df_tds, left join df_td, left join with df_tr
-df_tds.join(df_td, df_tds["driverid"] == df_td["driverid"])\
-        .join(df_tr, df_tds["raceid"] == df_tr["raceid"])
-
-# Select only relavent columns 
-df_tds.selectExpr('SELECT to_date("race_date", "MM/dd/yyyy") as "race_date", year("race_date") as "race_year", "position", "race_name", concat("forename", "surname") as "driver_name", "points", "driverstandingsid", "raceid"')
-
-# Write the data into s3 with glue catalog table enabled 
-df_tds.write.option("mode", "overwrite")\
-            .option("partitionBy", "race_year")\
-            .option("compression", "snappy")\
-            .save("s3://formulaonegc/f1_world_championship/fact/")
-
-# Code to Create / update glue catalog
+    print('# read the tabledriver_standings data')
+    gc_df_tds = glueContext.create_dynamic_frame_from_catalog(database="admin_prod", table_name="tabledriver_standings")
+    df_tds = gc_df_tds.toDF()
+    
+    print('# read the tabledrivers')
+    gc_df_td = glueContext.create_dynamic_frame_from_catalog(database="admin_prod", table_name="tabledrivers")
+    df_td = gc_df_td.toDF()
+    
+    print('# read the tableraces')
+    gc_df_tr = glueContext.create_dynamic_frame_from_catalog(database="admin_prod", table_name="tableraces")
+    df_tr = gc_df_tr.toDF()
+    
+    print('# Rename some generic column names')
+    df_tr = df_tr.withColumnsRenamed({"date":"race_date", "name":"race_name"})
+    
+    print('# df_tds, left join df_td, left join with df_tr')
+    df = df_tds.join(df_td, "driverid", "left")\
+        .join(df_tr, "raceid", "left")
+    
+    print('# Select only relavent columns')
+    df = df.selectExpr("race_date", "year(race_date) as race_year", "position", "race_name", "concat(forename, surname) as driver_name", "points", "df_tds.driverstandingsid", "df_tds.raceid")
+    
+    print('# Write the data into s3 with glue catalog table enabled') 
+    df.write.option("mode", "overwrite")\
+                .option("partitionBy", "race_year")\
+                .option("compression", "snappy")\
+                .save("s3://formulaonegc/f1_world_championship/fact/")
+    
+    print('# Code to Create / update glue catalog')
+    
+except Exception as e:
+    print(e)
+    raise e
